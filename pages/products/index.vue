85a5b1f785c783c3cd5daf5353014f7f913d5105
mark <markpackham1@gmail.com>
2022-11-16 12:42:17 +0000

    demo routes
    https://www.youtube.com/watch?v=QS8MwC8S4o8&list=PL4cUxeGkcC9haQlqdCQyYmL_27TesCGPC&index=4

---
+++ pages/products/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2>Products</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quas rerum deleniti culpa et? Eius.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quas rerum deleniti culpa et? Eius.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+
+p {
+    margin: 20px 0;
+}
+</style>
