591fda86993f50970a0f20480f31efcc4ff6c8ef
mark <markpackham1@gmail.com>
2022-11-16 13:19:10 +0000

    demo route paramater
    https://www.youtube.com/watch?v=X9ta1grG1j0&list=PL4cUxeGkcC9haQlqdCQyYmL_27TesCGPC&index=5

---
+++ pages/products/[id].vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>Product detials for {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis est accusantium modi porro tenetur
+            quisquam.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
