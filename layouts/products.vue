1d2174eb044c86eaf663973c002de90a023c00e2
mark <markpackham1@gmail.com>
2022-11-16 13:57:18 +0000

    use definePageMeta for specific page layouts
    https://www.youtube.com/watch?v=dvanqBUoxhc&list=PL4cUxeGkcC9haQlqdCQyYmL_27TesCGPC&index=7

---
+++ layouts/products.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/products">Nuxt 3 Learn Merch</NuxtLink>
+            </nav>
+        </header>
+
+        <!-- output page content (like @yield in Laravel) -->
+        <div>
+            <slot />
+        </div>
+
+        <footer>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/products">Products</NuxtLink>
+                </li>
+            </ul>
+        </footer>
+    </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+    color: #12b488;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,9 @@
 </template>
 
 <script setup>
-
+definePageMeta({
+    layout: 'products'
+})
 </script>
 
 <style scoped>
