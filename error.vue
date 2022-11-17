b8798d3974231c10a066064e246bc2d7a8e8a894
mark <markpackham1@gmail.com>
2022-11-17 14:34:48 +0000

    create custom errors page

---
+++ error.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="mt-7 max-w-sm mx-auto text-center card">
+        <p class="mt-7 text-7xl font-bold">
+            {{ error.statusCode }}
+        </p>
+        <div class="mt-7 text-6xl">Error</div>
+        <div class="mt-7">
+            {{ error.message }}
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps(['error'])
+</script>
