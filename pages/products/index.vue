1094c26f36391b0b95e2b15254ecc36d682c1898
mark <markpackham1@gmail.com>
2022-11-18 11:00:35 +0000

    override global meta info
    https://youtu.be/MA76RDwfjH8?t=370

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -16,6 +16,13 @@ definePageMeta({
 // grab products from a fake store
 const { data: products } = await useFetch('https://fakestoreapi.com/products')
 
+useHead({
+    title: 'Nuxt Learn 3 | Merch',
+    meta: [
+        { name: 'description', content: 'Nuxt 3 Merch' }
+    ]
+})
+
 </script>
 
 <style scoped>
