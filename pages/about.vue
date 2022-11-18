eed0ec0fc563d06a85bb0a21c9389334e111aa7b
mark <markpackham1@gmail.com>
2022-11-18 11:21:10 +0000

    get data from backend server

--- pages/about.vue
+++ pages/about.vue
@@ -3,11 +3,12 @@
         <h2>About</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quas rerum deleniti culpa et? Eius.</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quas rerum deleniti culpa et? Eius.</p>
+        <div>{{ data.message }}</div>
     </div>
 </template>
 
 <script setup>
-
+const { data } = await useFetch('/api/ninja')
 </script>
 
 <style scoped>
---
+++ server/api/ninja.js
@@ -0,0 +1,7 @@
+// This is the backend server
+
+export default defineEventHandler(() => {
+  return {
+    message: `Hello World I am from the server!`,
+  };
+});
